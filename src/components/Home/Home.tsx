e8b1fda20d04ac705f4632bc6713a36496b1d0b6
IvanKorneev <ivan.kornieiev@gmail.com>
2023-01-11 11:43:53 +0200

    received data from the server

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,4 +1,16 @@
+import {useEffect, useState} from "react";
+import axios from "axios";
+
 const Home = () => {
+
+    const URL_ARTIST = ' http://localhost:3001/artists';
+    const [artists, setArtists] = useState('');
+
+    useEffect(() => {
+        axios.get(URL_ARTIST).then(response => {
+            setArtists(response.data)
+        })
+    }, [])
     return (
         <div>
             Home
