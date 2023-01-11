224e5b2e51dc758fee1d6886df6db289a9a19fc7
IvanKorneev <ivan.kornieiev@gmail.com>
2023-01-11 11:47:52 +0200

    add component Header

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,10 @@
+import {Link} from "react-router-dom";
+
+const Header = () => {
+    return (
+        <header className='Header'>
+            <Link to="/">Music db</Link>
+        </header>
+    )
+}
+export default Header;
