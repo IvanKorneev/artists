7faed29a409a84682a001a5cdc3fc0e7d926e7eb
IvanKorneev <ivan.kornieiev@gmail.com>
2023-01-18 12:22:43 +0200

    create function List and render Artist

--- src/components/ArtistList/ArtistList.tsx
+++ src/components/ArtistList/ArtistList.tsx
@@ -1,7 +1,28 @@
 import './ArtistList.sass'
-const ArtistList = () => {
+import {Artist} from "../Types/Artist";
+import {Link} from "react-router-dom";
+
+interface ArtistListProps {
+    allArtists: Artist[]
+}
+
+const list = (allArtists: Artist[]) => {
+    return allArtists ? allArtists.map(item => (<Link key={item.id} to={`/artist${item.id}`} className='artist_item'
+    style={{background:`url('/images/covers/${item.cover}.jpg') no-repeat`}}>
+        <div>
+            {item.name}
+        </div>
+    </Link>)) : null;
+}
+
+const ArtistList = ({allArtists}: ArtistListProps) => {
     return (
-        <div>Artist</div>
+        <div className='artists_list'>
+            <h4>Browse the artists</h4>
+            <div className='artists_container'>
+                {list(allArtists)}
+            </div>
+        </div>
     );
 };
 export default ArtistList;
--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -2,11 +2,13 @@ import {useEffect, useState} from "react";
 import axios from "axios";
 import Header from "../Header/Header";
 import Banner from "../Banner/Banner";
+import ArtistList from "../ArtistList/ArtistList";
+import {Artist} from "../Types/Artist";
 
 const Home = () => {
 
     const URL_ARTIST = ' http://localhost:3001/artists';
-    const [artists, setArtists] = useState('');
+    const [artists, setArtists] = useState<Artist[]>([]);
 
     useEffect(() => {
         axios.get(URL_ARTIST).then(response => {
@@ -17,6 +19,7 @@ const Home = () => {
         <div>
             <Header/>
             <Banner/>
+            <ArtistList allArtists={artists}/>
         </div>
     );
 };
